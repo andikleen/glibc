1cc0eb14d1e4c58898e251e01b2ea901cfed9659
Andi Kleen <ak@linux.intel.com>
2012-11-10 00:53:54 -0800

    Extend the test suite for TSX and add some new tests to test elision
    - Some of the existing mutex tests do not work with lock elision
    semantics. We run them with PTHREAD_MUTEX=none PTHREAD_RWLOCK=none,
    but do not otherwise modify it.
    
    This is expected behaviour, related to RTM lock elision's inability
    to track the owner of lock (see the original description for more
    details). It's not expected that these changes will break any programs,
    it's mostly related to obscure behaviour.
    - I added some ifdefs to these tests and add new variants that
    set the defines to test elision behaviour.
    - Add new tests that explicitely check for elision.
    Note strictly this violates the TSX spec which never guarantees any transaction
    ever succeeding. However we assume simple transactions will succeed and will
    fail if not.
    - Add tests covering the new lock kinds.
    
    2013-01-30  Andi Kleen  <ak@linux.intel.com>
    
    	* Makefile: Add elision tests. Disable elision for some existing
            tests.
    	* tst-abstime.c: Fix for elision.
    	* tst-abstime1b.c: New file for lock elision code.
    	* tst-abstime1c.c:dito.
    	* tst-elision-common.c: dito.
    	* tst-elision1.c: dito.
    	* tst-elision1b.c: dito.
    	* tst-elision2.c: dito.
    	* tst-initializers2-c89.c: Handle lock elision
    	* tst-initializers2-c99.c: dito.
    	* tst-initializers2-gnu89.c: dito.
    	* tst-initializers2-gnu99.c: dito.
    	* tst-initializers2.c: dito.
    	* tst-mutex5.c: Handle lock elision.
    	* tst-mutex5b.c: New file.
    	* tst-mutex5c.c: dito.
    	* tst-mutex5d.c: dito
    	* tst-mutex5e.c: dito.
    	* tst-mutex7b.c: dito.
    	* tst-mutex7c.c: dito.
    	* tst-mutex7d.c: dito.
    	* tst-mutex7e.c: dito.
    	* tst-mutex8.c: Handle lock elision.
    	* tst-mutex8b.c: New file.
    	* tst-mutex8c.c: dito.
    	* tst-mutex8d.c: dito.
    	* tst-mutex8e.c: dito.
    	* tst-mutex8f.c: dito.
    	* tst-mutex8g.c: dito.

---
+++ nptl/tst-mutex8e.c
@@ -0,0 +1,3 @@
+#define NAME "adaptive not elided"
+#define TYPE PTHREAD_MUTEX_ADAPTIVE_NP|PTHREAD_MUTEX_NO_ELISION_NP
+#include "tst-mutex8.c"
---
+++ nptl/tst-mutex8g.c
@@ -0,0 +1,5 @@
+#include <elision-conf.h>
+#ifdef SUPPORTS_ELISION
+#define ELIDED 1
+#endif
+#include "tst-mutex8.c"
