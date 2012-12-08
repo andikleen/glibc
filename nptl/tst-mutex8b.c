075508d40071ff7f1218f52f21ef4e731ae586c5
Andi Kleen <ak@linux.intel.com>
2012-12-08 07:34:12 -0800

    add tst-mutex8*.c extra tests

---
+++ nptl/tst-mutex8b.c
@@ -0,0 +1,4 @@
+#define NAME "timed elided"
+#define ELIDED 1
+#define TYPE PTHREAD_MUTEX_TIMED_ELISION_NP
+#include "tst-mutex8.c"
---
+++ nptl/tst-mutex8c.c
@@ -0,0 +1,4 @@
+#define NAME "adaptive elided"
+#define ELIDED 1
+#define TYPE PTHREAD_MUTEX_ADAPTIVE_ELISION_NP
+#include "tst-mutex8.c"
