1cac2aa779efbecf43e80b1b82414e0f4745a690
Andi Kleen <ak@linux.intel.com>
2012-12-22 01:03:04 -0800

    Add elision to pthread_mutex_{try,timed,un,}lock
    Add elision paths to the basic mutex locks.
    
    The normal path has a check for RTM and upgrades the lock
    to RTM when available. Trylocks cannot automatically upgrade,
    so they check for elision every time.
    
    We use a 4 byte value in the mutex to store the lock
    elision adaptation state. This is separate from the adaptive
    spin state and uses a separate field.
    
    Condition variables currently do not support elision.
    
    Recursive mutexes and condition variables may be supported at some point,
    but are not in the current implementation. Also "trylock" will
    not automatically enable elision unless some other lock call
    has been already called on the lock.
    
    This version does not use IFUNC, so it means every lock has one
    additional check for elision. Benchmarking showed the overhead
    to be negligible.
    
    2013-06-24  Andi Kleen  <ak@linux.intel.com>
                Hongjiu Lu  <hongjiu.lu@intel.com>
    
    	* pthread_mutex_lock.c (adaptive_lock): Add
    	(__pthread_mutex_lock): Add lock elision support.
    	* pthread_mutex_timedlock.c (pthread_mutex_timedlock): dito.
    	* pthread_mutex_trylock.c (__pthread_mutex_trylock): dito.
    	* pthread_mutex_unlock.c (__pthread_mutex_unlock_usercnt): dito.
    	* sysdeps/unix/sysv/linux/pthread_mutex_cond_lock.c: dito.
    	* sysdeps/unix/sysv/linux/x86/bits/pthreadtypes.h: dito.
    	* sysdeps/unix/sysv/linux/x86/Makefile: New file.
    	* sysdeps/unix/sysv/linux/x86/force-elision.h: New file
    	* sysdeps/unix/sysv/linux/x86/pthread_mutex_cond_lock.c: dito.
    	* sysdeps/unix/sysv/linux/x86/pthread_mutex_lock.c: dito.
    	* sysdeps/unix/sysv/linux/x86/pthread_mutex_timedlock.c: dito.
    	* sysdeps/unix/sysv/linux/x86/pthread_mutex_trylock.c: dito.
    	* sysdeps/unix/sysv/linux/x86/pthread_mutex_unlock.c: dito.
    	* pthreadP.h: (PTHREAD_MUTEX_UPGRADED_ELISION_NP): Add.

--- nptl/pthread_mutex_unlock.c
+++ nptl/pthread_mutex_unlock.c
@@ -23,6 +23,10 @@
 #include <lowlevellock.h>
 #include <stap-probe.h>
 
+#ifndef lll_unlock_elision
+#define lll_unlock_elision(a,b) ({ lll_unlock (a,b); 0; })
+#endif
+
 static int
 internal_function
 __pthread_mutex_unlock_full (pthread_mutex_t *mutex, int decr)
@@ -34,8 +38,9 @@ __pthread_mutex_unlock_usercnt (mutex, decr)
      pthread_mutex_t *mutex;
      int decr;
 {
-  int type = PTHREAD_MUTEX_TYPE (mutex);
-  if (__builtin_expect (type & ~PTHREAD_MUTEX_KIND_MASK_NP, 0))
+  int type = PTHREAD_MUTEX_TYPE_ELISION (mutex);
+  if (__builtin_expect (type &
+		~(PTHREAD_MUTEX_KIND_MASK_NP|PTHREAD_MUTEX_ELISION_FLAGS_NP), 0))
     return __pthread_mutex_unlock_full (mutex, decr);
 
   if (__builtin_expect (type, PTHREAD_MUTEX_TIMED_NP)
@@ -55,6 +60,13 @@ __pthread_mutex_unlock_usercnt (mutex, decr)
 
       return 0;
     }
+  else if (__builtin_expect (type == PTHREAD_MUTEX_TIMED_ELISION_NP, 1)
+           || (type == PTHREAD_MUTEX_ADAPTIVE_ELISION_NP))
+    {
+      /* Don't reset the owner/users fields for elision.  */
+      return lll_unlock_elision (mutex->__data.__lock,
+				      PTHREAD_MUTEX_PSHARED (mutex));
+    }
   else if (__builtin_expect (type == PTHREAD_MUTEX_RECURSIVE_NP, 1))
     {
       /* Recursive mutex.  */
@@ -66,7 +78,9 @@ __pthread_mutex_unlock_usercnt (mutex, decr)
 	return 0;
       goto normal;
     }
-  else if (__builtin_expect (type == PTHREAD_MUTEX_ADAPTIVE_NP, 1))
+  type &= ~PTHREAD_MUTEX_ELISION_FLAGS_NP;
+  if (__builtin_expect (type == PTHREAD_MUTEX_ADAPTIVE_NP, 1) ||
+      __builtin_expect (type == PTHREAD_MUTEX_TIMED_NP, 1))
     goto normal;
   else
     {
