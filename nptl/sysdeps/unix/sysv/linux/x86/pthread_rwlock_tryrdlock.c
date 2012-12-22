6440bc49ea8b5bac2dc5d3b95b94f9c455dbbaef
Andi Kleen <ak@linux.intel.com>
2012-12-22 01:09:07 -0800

    Add lock elision to rwlocks
    This code is very similar to the main pthread mutex code, but we have
    to implement part of it in assembler. IFUNC is used to avoid any
    fast path overhead on non RTM systems for the lock functions (but not
    unlock)
    
    The patch kit is large because there are so many combinations
    of reader and writer locks, but it is fairly repetive.
    
    The basic elision algorithm is very similar to normal mutexes:
    a wrapper checks if the lock is free (both reader and writer)
    We have to check both reader and writer because separating them
    would require putting state on different cache lines.
    
    The algorithm is very basic currently and may be tuned
    more in the future.
    
    No adaptation at this point, the elision will not automatically
    disable itself. The programmer or user can do it explicitely though.
    
    New lock type flags are exposed to force enable/disable elision
    for a specific rwlocks.
    
    We use some padding in the internal rwlock as a elision flag.
    This is 8 bytes on 64bits currently, but could be made smaller later
    without compatibility problems (or to share some bytes with a
    adaptation count). 32bit uses one byte.
    
    2013-01-30  Andi Kleen <ak@linux.intel.com>
                Hongjiu Lu <hongjiu.lu@intel.com>
    
    	* pthread_rwlock_init.c (__pthread_rwlock_init): Support elision.
    	* pthread_rwlockattr_setkind_np.c (pthread_rwlockattr_setkind_np): Support elision.
    	* sysdeps/unix/sysv/linux/i386/i486/pthread_rwlock_rdlock.S: dito.
    	* sysdeps/unix/sysv/linux/i386/i486/pthread_rwlock_timedrdlock.S: dito
    	* sysdeps/unix/sysv/linux/i386/i486/pthread_rwlock_timedwrlock.S: dito.
    	* sysdeps/unix/sysv/linux/i386/i486/pthread_rwlock_unlock.S: dito.
    	* sysdeps/unix/sysv/linux/i386/i486/pthread_rwlock_wrlock.S: dito.
    	* sysdeps/unix/sysv/linux/internaltypes.h (struct pthread_condattr):
    	(struct pthread_rwlockattr): Support elision flags in internal rwlock.
    	* sysdeps/unix/sysv/linux/lowlevelrwlock.sym: Add ELIDED.
    	* sysdeps/unix/sysv/linux/x86/bits/pthreadtypes.h: Add elision flags
    	* sysdeps/unix/sysv/linux/x86/pthread_rwlock_tryrdlock.c: New file
    	* sysdeps/unix/sysv/linux/x86/pthread_rwlock_trywrlock.c: dito.
    	* sysdeps/unix/sysv/linux/x86_64/pthread_rwlock_rdlock.S: Handle elision.
    	* sysdeps/unix/sysv/linux/x86_64/pthread_rwlock_timedrdlock.S: dito.
    	* sysdeps/unix/sysv/linux/x86_64/pthread_rwlock_timedwrlock.S: dito.
    	* sysdeps/unix/sysv/linux/x86_64/pthread_rwlock_unlock.S: dito.
    	* sysdeps/unix/sysv/linux/x86_64/pthread_rwlock_wrlock.S: dito.

---
+++ nptl/sysdeps/unix/sysv/linux/x86/pthread_rwlock_tryrdlock.c
@@ -0,0 +1,52 @@
+/* pthread_rwlock_tryrdlock: Lock eliding version of pthreads rwlock_tryrdlock.
+   Copyright (C) 2013 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>. */
+#include <pthread.h>
+#include <pthreadP.h>
+#include <hle.h>
+#include "elision-conf.h"
+#include "init-arch.h"
+
+#define __pthread_rwlock_tryrdlock __full_pthread_rwlock_tryrdlock
+#include <nptl/pthread_rwlock_tryrdlock.c>
+#undef __pthread_rwlock_tryrdlock
+
+int
+__pthread_rwlock_tryrdlock(pthread_rwlock_t *rwlock)
+{
+  unsigned status;
+
+  if ((rwlock->__data.__eliding > 0 && __elision_available)
+      || (rwlock->__data.__eliding == 0 && __rwlock_rtm_enabled))
+    {
+      if ((status = _xbegin()) == _XBEGIN_STARTED)
+	{
+	  if (rwlock->__data.__writer == 0
+	      && rwlock->__data.__nr_readers == 0)
+	    return 0;
+	  /* Lock was busy. Fall back to normal locking.
+	     Could also _xend here but xabort with 0xff code
+	     is more visible in the profiler. */
+	  _xabort (0xff);
+	}
+      /* Aborts come here */
+    }
+
+  return __full_pthread_rwlock_tryrdlock (rwlock);
+}
+
+strong_alias(__pthread_rwlock_tryrdlock, pthread_rwlock_tryrdlock);
