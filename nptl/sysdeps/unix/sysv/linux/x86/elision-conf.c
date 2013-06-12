938cb77a36bf5cf7009bc6288261d25517ceff6c
Andi Kleen <ak@linux.intel.com>
2013-06-12 08:10:10 -0700

    Add environment variable tuning for elision parameters.
    Add tunables for enabling/disabling elision and tuning the elision retry
    and skip counts.
    
    This builds on the glibc-var infrastructure added earlier.
    
    nptl/:
    2013-06-18    Andi Kleen <ak@linux.intel.com>
    
    	* sysdeps/unix/sysv/linux/x86/elision-conf.c (struct elision_config): Add.
    	(struct tune): dito.
    	(complain): New function to write error message.
    	(elision_aconf_setup): New function to parse parameters.
    	(elision_mutex_init): New function to parse GLIBC_PTHREAD_MUTEX.
    	(elision_rwlock_init): New function to parse GLIBC_PTHREAD_RWLOCK.
    	(elision_init): Call elision_mutex_init and elision_rwlock_init.
    
    /:
    2013-06-18    Andi Kleen <ak@linux.intel.com>
    
    	* manual/elision.texi: Add sections describing environment variables.

--- nptl/sysdeps/unix/sysv/linux/x86/elision-conf.c
+++ nptl/sysdeps/unix/sysv/linux/x86/elision-conf.c
@@ -18,9 +18,10 @@
 
 #include "config.h"
 #include <pthreadP.h>
+#include <unistd.h>
 #include <init-arch.h>
 #include <elision-conf.h>
-#include <unistd.h>
+#include <glibc-var.h>
 
 struct elision_config __elision_aconf =
   {
@@ -29,6 +30,85 @@ struct elision_config __elision_aconf =
     .retry_try_xbegin = 3,
     .skip_trylock_internal_abort = 3,
   };
+
+struct tune
+{
+  const char *name;
+  unsigned offset;
+  int len;
+};
+
+#define FIELD(x) { #x, offsetof(struct elision_config, x), sizeof(#x)-1 }
+
+static const struct tune tunings[] =
+  {
+    FIELD(skip_lock_busy),
+    FIELD(skip_lock_internal_abort),
+    FIELD(retry_try_xbegin),
+    FIELD(skip_trylock_internal_abort),
+    {}
+  };
+
+#define PAIR(x) x, sizeof (x)-1
+
+/* Complain.  */
+
+static void
+complain (const char *msg, int len)
+{
+  INTERNAL_SYSCALL_DECL (err);
+  INTERNAL_SYSCALL (write, err, 3, 2, (char *)msg, len);
+}
+
+/* Parse configuration information.  */
+
+static void
+elision_aconf_setup (const char *s)
+{
+  int i;
+
+  while (*s)
+    {
+      for (i = 0; tunings[i].name != NULL; i++)
+	{
+	  int nlen = tunings[i].len;
+
+	  if (strncmp (tunings[i].name, s, nlen) == 0)
+	    {
+	      char *end;
+	      int val;
+
+	      if (s[nlen] != '=')
+		{
+  		  complain (PAIR ("pthreads: invalid GLIBC_PTHREAD_MUTEX syntax: missing =\n"));
+	 	  return;
+		}
+	      s += nlen + 1;
+	      val = strtoul (s, &end, 0);
+	      if (end == s)
+		{
+  		  complain (PAIR ("pthreads: invalid GLIBC_PTHREAD_MUTEX syntax: missing number\n"));
+	 	  return;
+		}
+	      *(int *)(((char *)&__elision_aconf) + tunings[i].offset) = val;
+	      s = end;
+	      if (*s == ',' || *s == ':')
+		s++;
+	      else if (*s)
+		{
+  		  complain (PAIR ("pthreads: invalid GLIBC_PTHREAD_MUTEX syntax: garbage after number\n"));
+	 	  return;
+		}
+	      break;
+	    }
+	}
+      if (tunings[i].name == NULL)
+      	{
+  	  complain (PAIR ("pthreads: invalid GLIBC_PTHREAD_MUTEX syntax: unknown tunable\n"));
+ 	  return;
+	}
+    }
+}
 
 /* Elided rwlock toggle.  */
 
@@ -46,6 +126,54 @@ int __elision_available attribute_hidden;
 
 int __pthread_force_elision attribute_hidden;
 
+/* Initialize mutex elision.  */
+
+static void
+elision_mutex_init (const char *s)
+{
+  if (s == NULL)
+    return;
+
+  if (strncmp (s, "elision", 7) == 0 && (s[7] == 0 || s[7] == ':'))
+    {
+      __pthread_force_elision = __elision_available;
+      if (s[7] == ':')
+        elision_aconf_setup (s + 8);
+    }
+  else if (strcmp (s, "none") == 0 || strcmp (s, "no_elision") == 0)
+    __pthread_force_elision = 0;
+  else
+    complain (PAIR ("pthreads: Unknown setting for GLIBC_PTHREAD_MUTEX\n"));
+}
+
+/* Initialize elision for rwlocks.  */
+
+static void
+elision_rwlock_init (const char *s)
+{
+  if (s == NULL)
+    return;
+  if (strncmp (s, "elision", 7) == 0)
+    {
+      __rwlock_rtm_enabled = __elision_available;
+      if (s[7] == ':')
+        {
+          char *end;
+	  int n;
+
+          n = strtoul (s + 8, &end, 0);
+	  if (end == s + 8)
+	    complain (PAIR ("pthreads: Bad retry number for GLIBC_PTHREAD_RWLOCK\n"));
+          else
+	    __rwlock_rtm_read_retries = n;
+	}
+    }
+  else if (strcmp (s, "none") == 0 || strcmp (s, "no_elision") == 0)
+    __rwlock_rtm_enabled = 0;
+  else
+    complain (PAIR ("pthreads: Unknown setting for GLIBC_PTHREAD_RWLOCK\n"));
+}
+
 /* Initialize elison.  */
 
 static void
@@ -58,6 +186,11 @@ elision_init (int argc __attribute__ ((unused)),
   __pthread_force_elision = __libc_enable_secure ? 0 : __elision_available;
   __rwlock_rtm_enabled = __libc_enable_secure ? 0 : __elision_available;
 #endif
+
+  /* For static builds need to call this explicitely. Noop for dynamic.  */
+  __glibc_var_init (argc, argv, environ);
+  elision_mutex_init (_dl_glibc_var[GLIBC_VAR_PTHREAD_MUTEX].val);
+  elision_rwlock_init (_dl_glibc_var[GLIBC_VAR_PTHREAD_RWLOCK].val);
 }
 
 #ifdef SHARED
